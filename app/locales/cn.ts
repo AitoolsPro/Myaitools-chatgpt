6ee7c96870e544ac8bbaba30bc5fec7814dce99f
AitoolsPro <kdf1688@gmail.com>
2023-03-28 00:45:57 +0800

    Your commit message

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -51,12 +51,9 @@ const cn = {
     },
     Avatar: "头像",
     Update: {
-      Version: (x: string) => `当前版本：${x}`,
-      IsLatest: "已是最新版本",
-      CheckUpdate: "检查更新",
-      IsChecking: "正在检查更新...",
-      FoundUpdate: (x: string) => `发现新版本：${x}`,
-      GoToUpdate: "前往更新",
+      Version: (x: string) => `ai资源导航网`,
+
+      FoundUpdate: (x: string) => `访问aitools.chat`,
     },
     SendKey: "发送键",
     Theme: "主题",
