4d8a63971268071cf7db2c1232b7562781b891c0
AitoolsPro <kdf1688@gmail.com>
2023-03-27 23:25:54 +0800

    Initial commit

---
+++ app/constant.ts
@@ -0,0 +1,5 @@
+export const OWNER = "Yidadaa";
+export const REPO = "ChatGPT-Next-Web";
+export const REPO_URL = "https://www.aitools.chat/";
+export const UPDATE_URL = "https://www.aitools.chat/";
+export const FETCH_COMMIT_URL = "https://www.aitools.chat/";
